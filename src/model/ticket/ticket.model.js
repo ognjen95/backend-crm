4d5a54a1b150f58f927910a87ba7675140a58833
ognjen95 <atlagicognjen@gmail.com>
2021-02-25 23:57:49 +0100

    all ticket routes finished

--- src/model/ticket/ticket.model.js
+++ src/model/ticket/ticket.model.js
@@ -27,4 +27,78 @@ const getTickets = (userId) => {
   });
 };
 
-module.exports = { insertTicket, getTickets };
+const getTicketById = (_id, userId) => {
+  return new Promise((resolve, reject) => {
+    try {
+      TicketSchema.find({ _id, userId })
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+  });
+};
+
+const ReplyMessage = ({ ticketId, userId, message, sender }) => {
+  return new Promise((resolve, reject) => {
+    try {
+      TicketSchema.findOneAndUpdate(
+        { _id: ticketId, userId },
+        {
+          status: 'Otvoren',
+          $push: {
+            conversation: { message, sender },
+          },
+        },
+        { new: true }
+      )
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+  });
+};
+
+const closeTicket = (_id, userId) => {
+  return new Promise((resolve, reject) => {
+    try {
+      TicketSchema.findOneAndUpdate(
+        { _id, userId },
+        {
+          status: 'Zatvoren',
+        },
+        { new: true }
+      )
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+  });
+};
+
+const deleteTicket = (_id, userId) => {
+  return new Promise((resolve, reject) => {
+    try {
+      TicketSchema.findOneAndDelete({ _id, userId })
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+  });
+};
+
+module.exports = {
+  insertTicket,
+  getTickets,
+  getTicketById,
+  ReplyMessage,
+  closeTicket,
+  deleteTicket,
+};
--- src/routers/ticket.router.js
+++ src/routers/ticket.router.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { insertTicket, getTickets } = require('../model/ticket/ticket.model');
+const {
+  insertTicket,
+  getTickets,
+  getTicketById,
+  ReplyMessage,
+  closeTicket,
+  deleteTicket,
+} = require('../model/ticket/ticket.model');
 const protect = require('../middleware/authMiddleware');
 
 router.all('/', (req, res, next) => {
@@ -64,7 +71,7 @@ router.post('/', protect, async (req, res) => {
 });
 
 //get all tickets , proteccted
-router.get('/', protect, async (req, res) => {
+router.get('/all-tickets', protect, async (req, res) => {
   try {
     const user = req.user; // from middleware
 
@@ -79,4 +86,100 @@ router.get('/', protect, async (req, res) => {
   }
 });
 
+//get all tickets , proteccted
+router.get('/:ticketId', protect, async (req, res) => {
+  try {
+    const ticketId = req.params.ticketId;
+    const user = req.user; // from middleware
+
+    const ticket = await getTicketById(ticketId, user._id);
+
+    res.json({ result: ticket });
+  } catch (error) {
+    res.json({
+      status: 'error',
+      msg: error.message,
+    });
+  }
+});
+
+//add new message
+router.put('/:ticketId', protect, async (req, res) => {
+  try {
+    const { sender, message } = req.body;
+
+    const ticketId = req.params.ticketId;
+    const user = req.user;
+    const userId = req.user._id; // from middleware
+
+    const ticket = await ReplyMessage({
+      ticketId,
+      userId,
+      message,
+      sender: user.name,
+    });
+
+    if (ticket._id) {
+      return res.json({ status: 'Message added, ticket updated.' });
+    }
+
+    res.json({ error: 'Could not reply' });
+  } catch (error) {
+    res.json({
+      status: 'error',
+      msg: error.message,
+    });
+  }
+});
+
+//Update ticket status closed
+
+router.patch('/:ticketId', protect, async (req, res) => {
+  try {
+    const ticketId = req.params.ticketId;
+
+    const user = req.user; // from middleware
+    const ticket = await getTicketById(ticketId, user._id);
+
+    console.log(ticket[0].status);
+    ticket[0].conversation.length < 2 &&
+      res.json({ status: 'You must answer first to close ticket' });
+
+    const ticketStatusClosed = await closeTicket(ticketId, user._id);
+
+    if (ticketStatusClosed.status.toLowerCase().trim() === 'zatvoren') {
+      return res.json({ status: 'Ticket is already closed' });
+    }
+
+    if (ticketStatusClosed._id) {
+      return res.json({ status: 'Ticket is closed sucessfuly' });
+    }
+
+    res.json({ error: 'Could not close ticket' });
+  } catch (error) {
+    res.json({
+      status: 'error',
+      msg: error.message,
+    });
+  }
+});
+
+//delete ticket
+
+router.delete('/:ticketId', protect, async (req, res) => {
+  try {
+    const ticketId = req.params.ticketId;
+
+    const user = req.user; // from middleware
+
+    await deleteTicket(ticketId, user._id);
+
+    return res.json({ status: 'Ticket is deleted.' });
+  } catch (error) {
+    res.json({
+      status: 'error',
+      msg: error.message,
+    });
+  }
+});
 module.exports = router;
